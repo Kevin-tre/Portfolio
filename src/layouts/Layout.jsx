3fdf92f628d6ebd4d9ec18d8bf75889ec9ce3ecc
Kevin-tre <kevintrepied@gmail.com>
2024-11-13 12:41:04 +0100

    fix

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./Header/Header";
 import Home from "./Home Page/Home";
 import Footer from "./Footer/Footer";
 
 const Layout = () => {
   return (
-    <Router>
+    <>
       <Header />
       <Home />
       <Footer />
-    </Router>
+    </>
   );
 };
 
