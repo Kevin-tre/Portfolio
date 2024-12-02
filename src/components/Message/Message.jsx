724cfa2ebb9538c19ac8932371ab479a55233424
Kevin-tre <kevintrepied@gmail.com>
2024-12-02 12:39:33 +0100

    maj msg

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -22,8 +22,8 @@ const Message = () => {
             }}
           >
             {isMobileOrTablet
-              ? "En 2023, j’ai choisi de devenir développeur front-end et suivi une formation en tant qu'Intégrateur web qui a permis de renforcer mes compétences dans plusieurs domaines."
-              : "En 2023, j’ai choisi de me reconvertir en développeur front-end. Après une auto-formation en HTML et CSS et une POEC de trois mois, j’ai rejoint la formation Intégrateur Web d’OpenClassrooms. J'y ai renforcé mes compétences en JavaScript, en optimisation et en création d'interfaces avec React, React-Router et Redux."}
+              ? "En 2023, j’ai choisi de devenir développeur front-end et suivi une formation en tant que Développeur web qui a permis de renforcer mes compétences dans plusieurs domaines."
+              : "En 2023, j’ai choisi de me reconvertir en développeur front-end. Après une auto-formation en HTML et CSS et une POEC de trois mois, j’ai rejoint la formation Développeur Web d’OpenClassrooms. J'y ai renforcé mes compétences en JavaScript, en optimisation et en création d'interfaces avec React, React-Router et Redux."}
           </TypeIt>
         </h1>
       </div>
